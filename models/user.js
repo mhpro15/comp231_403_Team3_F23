9743f1f5c3ac93f32c433296f336e4f2e4111e01
Hung Nguyen <nguyenmanhhungpro15@gmail.com>
2023-11-07 10:13:08 -0500

    user model schema

--- models/user.js
+++ models/user.js
@@ -1,1 +1,33 @@
 //test push by heba
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  uid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
