6c1b4782cf96bfaeb6a4bb8bfb24161ecbf2a7c8
Heba Sajid <hsajid35467@gmail.com>
2023-11-14 16:30:29 -0500

    Team model created - initial

---
+++ controllers/teamController.js
@@ -0,0 +1,76 @@
+let express = require("express");
+let router = express.Router();
+let mongoose = require("mongoose");
+let passport = require("passport");
+
+let teamSchema = require("../models/team");
+
+
+//GET
+module.exports.handleTeamList = async (req, res, next) => {
+  try {
+    let teamList = await teamSchema.find();
+    res.json({ status: true, data: teamList });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: false, msg: "Failed to Fetch Teams List" });
+  }
+};
+
+//CREATE
+module.exports.handleCreateTeam = async (req, res, next) => {
+  //get last uid
+  let lastTeam = await teamSchema.findOne().sort({ tid: -1 });
+  let newTeam = new teamSchema({
+    tid: Number(lastTeam.tid) + 1,
+    teamName: req.body.teamName,
+    leaderName: req.body.leaderName,
+    members: req.body.members,
+    hackathonName: req.body.hackathonName,
+  });
+  console.log(req.body);
+  try {
+    await newTeam.save();
+    res.json({ status: true, msg: "Team Created Successfully" });
+  } catch (err) {
+    console.log(err);
+    res.json({ status: false, msg: "Failed to Create Team" });
+  }
+};
+
+//UPDATE
+module.exports.handleUpdateTeam = (req, res, next) => {
+  let id = req.params.id;
+
+  let updateTeam = teamSchema({
+    _id: id,
+    tid: req.body.tid,
+    teamName: req.body.teamName,
+    leaderName: req.body.leaderName,
+    members: req.body.members,
+    hackathonName: req.body.hackathonName,
+  });
+
+  userSchema.updateOne({ _id: id }, updateTeam, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      res.json({ status: true, msg: "Team Updated Successfully" });
+    }
+  });
+};
+
+//DELETE
+module.exports.handleDeleteTeam = (req, res, next) => {
+  let id = req.params.id;
+
+  teamSchema.deleteOne({ _id: id }, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      res.json({ status: true, msg: "Team Deleted Successfully" });
+    }
+  });
+};
---
+++ models/team.js
@@ -0,0 +1,33 @@
+//test push by heba
+
+const mongoose = require("mongoose");
+
+const teamSchema = new mongoose.Schema({
+  tid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  teamName: {
+    type: String,
+    required: true,
+  },
+  leaderName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  members: {
+    type: String,
+    required: true,
+  },
+  hackathonName: {
+    type: String,
+    required: true,
+    
+  },
+});
+
+const User = mongoose.model("Team", teamSchema);
+
+module.exports = Team;
