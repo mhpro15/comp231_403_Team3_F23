cec22dbb7b4912c30d9e6dbca9d107b190dcd4f3
Heba Sajid <hsajid35467@gmail.com>
2023-11-27 15:10:16 -0500

    4 boxes per row - styling added to boxes

--- client/src/app/components/teamlist.js
+++ client/src/app/components/teamlist.js
@@ -3,12 +3,14 @@ import TeamCard from "./teamcard";
 
 export default function TeamList(props) {
   return (
+    <div class="filter-container">
     <div className="team-list">
       {props.teams.map((team) => (
         <div key={team.id} className="team-item">
           <TeamCard team={team} />
         </div>
       ))}
+    </div>
     </div>
   );
 }
